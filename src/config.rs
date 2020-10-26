aacbf5f128a33d37273d852db82a10d46d82cbcb
Dhs92 <forehand.alex@gmail.com>
2020-10-26 12:49:06 -0700

    Removed config entry added by mistake

--- src/config.rs
+++ src/config.rs
@@ -3,11 +3,10 @@ use std::fmt::Display;
 use std::net::ToSocketAddrs;
 
 #[derive(Debug, Deserialize, Serialize, PartialOrd, PartialEq)]
-pub struct Config<'s, A>
+pub struct Config<A>
 where
     A: ToSocketAddrs + Copy + Display,
 {
-    server_name: &'s str,
     address: A,
     thread_count: usize,
 }
