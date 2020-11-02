570afd4d31466066ded86580d0ef80b1d767fb16
Dhs92 <forehand.alex@gmail.com>
2020-11-01 23:38:51 -0800

    Moved driver code to platform specific files

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
-use config::Config;
-use log::LevelFilter;
-use serde_json::from_reader;
+#[cfg(not(debug_assertions))]
+use crate::os::CONFIG_PATH;
 use std::fmt::Display;
 #[cfg(not(debug_assertions))]
 use std::fs::create_dir;
-use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Error, Write};
 use std::net::{Shutdown, TcpListener, TcpStream, ToSocketAddrs};
 use std::process::exit;
-use thread_pool::ThreadPool;
 
 const SERVER_NAME: &str = "trash";
-const CONFIG_NAME: &str = "config.json";
-
-#[cfg(not(debug_assertions))]
-const CONFIG_PATH: &str = if cfg!(linux) {
-    "/etc/http-server"
-} else if cfg!(windows) {
-    "C:\\Program Files\\Common Files\\http-server\\"
-} else {
-    "config.json"
-};
-
-#[cfg(debug_assertions)]
-const CONFIG_PATH: &str = "./";
 
 mod config;
+#[cfg_attr(windows, path = "os/windows.rs")]
+#[cfg_attr(target_os = "linux", path = "os/linux.rs")]
+mod os;
 mod request;
 mod response;
 
@@ -34,51 +21,7 @@ mod response;
 // TODO more robust error handling
 
 fn main() {
-    simple_logging::log_to_stderr(LevelFilter::Error);
-    #[cfg(not(debug_assertions))]
-    match create_dir(CONFIG_PATH) {
-        Ok(_) => log::debug!("Created config directory: {}", CONFIG_PATH),
-        Err(e) => log::error!("Could not create config directory: {}", e),
-    }
-
-    let mut config_file = match OpenOptions::new()
-        .create(true)
-        .write(true)
-        .read(true)
-        .open(format!("{}/{}", CONFIG_PATH, CONFIG_NAME))
-    {
-        Ok(conf_file) => conf_file,
-        Err(e) => {
-            log::error!("Unable to create {}: {}", CONFIG_NAME, e);
-            exit(-0x1)
-        }
-    };
-
-    if let Ok(config_meta) = config_file.metadata() {
-        if config_meta.len() < 75 {
-            config_file
-                .write_all(
-                    serde_json::to_string_pretty(&Config::default())
-                        .unwrap()
-                        .as_bytes(),
-                )
-                .unwrap();
-        }
-    }
-
-    let config = load_config(config_file);
-    simple_logging::log_to_stderr(config.log_level());
-
-    let thread_pool = ThreadPool::new(config.thread_count());
-
-    let listener = bind(&config.address());
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => thread_pool.execute(move || handle_request(&mut stream)),
-            Err(e) => log::warn!("Connection failed: {}", e),
-        }
-    }
+    os::start();
 }
 
 fn bind<A>(addr: A) -> TcpListener
@@ -145,14 +88,4 @@ fn read_request(stream: &mut TcpStream) -> Result<String, Error> {
     }
 
     Ok(buf)
-}
-
-fn load_config(reader: File) -> config::Config {
-    match from_reader::<File, config::Config>(reader) {
-        Ok(config) => config,
-        Err(e) => {
-            log::error!("Failed to read config file: {}", e);
-            exit(-0x69)
-        }
-    }
 }
